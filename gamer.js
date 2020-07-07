6e01a10d69f9c644d1deee21a23fadc109d59a66
Marco <apecuca@gmail.com>
2020-07-07 13:39:59 -0300

    initial commit

---
+++ gamer.js
@@ -0,0 +1,112 @@
+function onCreate(){
+    var x = document.getElementById("inputMT");
+    var y = document.getElementById("inputKG");
+    var z = document.getElementById("inputIMC");
+    var clean = document.getElementById("buttClean");
+    var calc = document.getElementById("buttCalc");
+    setTimeout(() => { x.style = "transform: scale(1.15);"; }, 0);
+    setTimeout(() => { x.style = "transform: scale(1);"; }, 200);
+    setTimeout(() => { y.style = "transform: scale(1.15);"; }, 200);
+    setTimeout(() => { y.style = "transform: scale(1);"; }, 400);
+    setTimeout(() => { z.style = "transform: scale(1.15);"; }, 400);
+    setTimeout(() => { z.style = "transform: scale(1);"; }, 600);
+    setTimeout(() => { clean.style = "transform: scale(1.15);"; }, 600);
+    setTimeout(() => { clean.style = "transform: scale(1);"; }, 800);
+    setTimeout(() => { calc.style = "transform: scale(1.15);"; }, 800);
+    setTimeout(() => { calc.style = "transform: scale(1);"; }, 1000);
+}
+
+function calcular(){
+    var altura = document.getElementById("inputMT").value;
+    let alturaS = document.getElementById("inputMT");
+    var peso = document.getElementById("inputKG").value;
+    let pesoS = document.getElementById("inputKG")
+    var IMC = document.getElementById("inputIMC");
+    var calculista = document.getElementById("buttCalc");
+    let calc = peso/((altura/100)*(altura/100));
+    try {
+        if(altura.length==0 || peso.length==0){
+            if(altura.length==0){
+                calculista.style = "border-color: red;";
+                alturaS.style = "border-color: red";
+                setTimeout(() => {alturaS.style = "border-color: black;"}, 600);
+                setTimeout(() => {calculista.style = "border-color: #008CBA;"}, 600);
+            }
+            if(peso.length==0){
+                calculista.style = "border-color: red;";
+                pesoS.style = "border-color: red";
+                setTimeout(() => {pesoS.style = "border-color: black;"}, 600);
+                setTimeout(() => {calculista.style = "border-color: #008CBA;"}, 600);
+            }        
+        }else{
+            calculista.style = "border-color: green;";
+            setTimeout(() => {calculista.style = "border-color: #008CBA;"}, 600);
+            IMC.value = calc;
+        }
+        if(calc<18.5){
+            document.getElementById("IMC01").style = "background-color: rgba(255, 255, 255, 0.5);";
+            setTimeout(() => { document.getElementById("IMC01").style = "background-color: #282a2f;"; }, 1000);
+        }
+        if(calc>18.5 && calc<24.9){
+            document.getElementById("IMC02").style = "background-color: rgba(255, 255, 255, 0.5);";
+            setTimeout(() => { document.getElementById("IMC02").style = "background-color: #282a2f;"; }, 1000);
+        }
+        if(calc>25 && calc<29.9){
+            document.getElementById("IMC1").style = "background-color: rgba(255, 255, 255, 0.5);";
+            setTimeout(() => { document.getElementById("IMC1").style = "background-color: #282a2f;"; }, 1000);
+        }
+        if(calc>30 && calc<39.9){
+            document.getElementById("IMC2").style = "background-color: rgba(255, 255, 255, 0.5);";
+            setTimeout(() => { document.getElementById("IMC2").style = "background-color: #282a2f;"; }, 1000);
+        }
+        if(calc>40){
+            document.getElementById("IMC3").style = "background-color: rgba(255, 255, 255, 0.5);";
+            setTimeout(() => { document.getElementById("IMC3").style = "background-color: #282a2f;"; }, 1000);
+        }
+    } catch (error) {
+        alert("ih");
+    }
+}
+
+function limpar(){
+    var altura = document.getElementById("inputMT").value;
+    let alturaS = document.getElementById("inputMT");
+    var peso = document.getElementById("inputKG").value;
+    let pesoS = document.getElementById("inputKG")
+    var clean = document.getElementById("buttClean");
+    var IMC = document.getElementById("inputIMC");
+    try {
+        if(altura.length==0 || peso.length==0){
+            if(altura.length==0){
+                alturaS.style = "border-color: red";
+                setTimeout(() => {alturaS.style = "border-color: black;"}, 600);
+            }
+            if(peso.length==0){
+                pesoS.style = "border-color: red";
+                setTimeout(() => {pesoS.style = "border-color: black;"}, 600);
+            }        
+        }else{
+            clean.style = "border-color: green;";
+            setTimeout(() => {clean.style = "border-color: #008CBA;"}, 600);
+            alturaS.value = "";
+            pesoS.value = "";
+            IMC.value = "";
+        }
+    } catch (error) {
+        alert("ih");
+    }
+}
+
+function changeBack(tipo){
+    var audiouwu = document.getElementById("myAudio");
+    if(tipo==1){
+        document.body.style = "background-image: url('imagens/backgroundofda.gif')";
+        document.getElementById("tudimtudim").style = "opacity: 0%";
+        audiouwu.load();
+        audiouwu.play();
+    }else{
+        document.body.style.backgroundImage = "url('imagens/background.png')";
+        document.getElementById("tudimtudim").style = "opacity: 100%";
+        audiouwu.pause();
+    }
+}

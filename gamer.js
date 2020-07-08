e3f2600a8444ef0beffe38d1b572223cb606bae2
Marco <apecuca@gmail.com>
2020-07-08 15:02:53 -0300

    Atualizado número de versão.

--- gamer.js
+++ gamer.js
@@ -1,5 +1,5 @@
 function onCreate(){
-    console.log("Ver 1.12 -> final <-");
+    console.log("Ver 1.17 -> final <-");
     var x = document.getElementById("inputMT");
     var y = document.getElementById("inputKG");
     var z = document.getElementById("inputIMC");
